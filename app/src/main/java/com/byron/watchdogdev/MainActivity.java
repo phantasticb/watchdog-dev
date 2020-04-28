8023d365b3c3a17c961dd514ecc4d955eae3c903
Byron Phan <phantasticb@gmail.com>
2020-04-28 13:40:15 -0400

    API implementation

--- app/src/main/java/com/byron/watchdogdev/MainActivity.java
+++ app/src/main/java/com/byron/watchdogdev/MainActivity.java
@@ -5,11 +5,23 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -19,9 +31,16 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class MainActivity extends AppCompatActivity {
+    // Alpha Vantage API key
+    private final String ALPHA_VANTAGE_KEY = "7E10Z3LOK5J917QU";
+
     // Watchlist
-    private LinkedHashMap<String, ArrayList<Float>> symbols = new LinkedHashMap<>();
+    private LinkedHashMap<String, ArrayList<Double>> symbols = new LinkedHashMap<>();
+
+    // Create a queue for API requests
+    private RequestQueue requestQueue;
 
     // Views
     private Button buttonAdd;
@@ -39,27 +58,118 @@ public class MainActivity extends AppCompatActivity {
         symbolEntry = (TextView) findViewById(R.id.symbolEntry);
         mainDisplay = (RecyclerView) findViewById(R.id.mainDisplay);
 
+        // Set click listener of button
         buttonAdd.setOnClickListener(new addButtonListener());
 
         mainDisplay.setLayoutManager(new LinearLayoutManager(this));
         stockListAdapter = new StockListAdapter(symbols);
         mainDisplay.setAdapter(stockListAdapter);
 
+        // Request Queue
+        requestQueue  = Volley.newRequestQueue(this);
+
     }
 
+    // Method that sends API request for market data
+    private void getPriceData(String symbol) {
+        // Put the URL into
+        String url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" +
+                symbol + "&apikey=" + ALPHA_VANTAGE_KEY;
+
+        // Tag variable for debug log purposes
+        final String REQUEST_TAG = "PRICE REQUEST";
+
+        // Here we go, this is such a mess, create a request object and fill in args
+        Request request = new JsonObjectRequest(Request.Method.GET, url, null,
+                // Implement this abstract listener class
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Get the quote data and parse it into an object since data is
+                        // contained within an inner object for Alpha Vantage
+                        JSONObject quote = response.optJSONObject("Global Quote");
+
+                        // Default ticker set to NULL, if it is still NULL we have a problem
+                        String symbol = "NULL";
+
+                        // optString tends to break for some reason so I shoved it into a try-catch
+                        try {
+                            // If it works, it sets the ticker to the actual symbol entered
+                            symbol = quote.optString("01. symbol");
+                        } catch (Exception e) {
+                            // If not, it pushes this to the log
+                            Log.v(REQUEST_TAG, "Failed to parse name.");
+                        }
+
+                        // If symbol successfully parsed from JSON, execute this
+                        if(symbol != "NULL") {
+                            // Create an ArrayList object to update the hashmap watchlist with
+                            ArrayList<Double> data = new ArrayList<>(2);
+
+                            // Updates price and change from JSON
+                            data.add(quote.optDouble("05. price"));
+                            data.add(quote.optDouble("09. change"));
+
+                            // Update debug log
+                            Log.v(REQUEST_TAG, "Successfully requested " + symbol + " data");
+                            Log.v(REQUEST_TAG, "Price is $" + data.get(0));
+
+                            // Add data to hashmap watchlist
+                            symbols.put(symbol, data);
+
+                            // Update debug log
+                            Log.v(REQUEST_TAG, symbols.toString());
+
+                            // Tell the adapter to update, possible redundancy here but idk
+                            mainDisplay.getAdapter().notifyItemInserted(symbols.size());
+                            mainDisplay.getAdapter().notifyDataSetChanged();
+                        } else {
+                            // If symbol failed to parse
+                            Log.v(REQUEST_TAG, "Failed to update price");
+
+                            // Show an error message
+                            Toast toast = Toast.makeText(getApplicationContext(),
+                                    "Invalid symbol", Toast.LENGTH_LONG);
+
+                            toast.show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    // Implement the required ErrorListener abstract class
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(REQUEST_TAG, "Request failed");
+
+                        // Just show an error message if something broke on a deep level
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Invalid symbol", Toast.LENGTH_LONG);
+
+                        toast.show();
+                    }
+                });
+
+        // Add the request (the whole mess above) to the queue
+        requestQueue.add(request);
+    }
+
+    // This is the listener for the add button
     private class addButtonListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
+
+            // If the text entry field isn't empty
             if(symbolEntry.getText().toString().trim().length() != 0) {
-                // Add the symbol to the list with a placeholder price data
-                ArrayList<Float> price_data = new ArrayList<>(2);
-                price_data.add(0F); price_data.add(0F);
-                symbols.put(symbolEntry.getText().toString().toUpperCase(), price_data);
+                // Ensure text is capitalized
+                String symbol = symbolEntry.getText().toString().toUpperCase();
 
-                mainDisplay.getAdapter().notifyItemInserted(symbols.size());
+                // Call the getPriceData function
+                getPriceData(symbol);
 
+                // Clear the entry field
                 symbolEntry.setText("");
+
+                // Update the log
                 Log.v("Main Activity", symbols.toString());
             }
         }
--- app/src/main/java/com/byron/watchdogdev/StockListAdapter.java
+++ app/src/main/java/com/byron/watchdogdev/StockListAdapter.java
@@ -17,15 +17,19 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class StockListAdapter extends RecyclerView.Adapter<StockListAdapter.StockViewHolder> {
-    private final LinkedHashMap<String, ArrayList<Float>> symbols;
+    // Include a variable for watchlist collected in input field to be passed to
+    private final LinkedHashMap<String, ArrayList<Double>> symbols;
 
-    public StockListAdapter(LinkedHashMap<String, ArrayList<Float>> data) {
+    // Constructor passes watchlist data to member variable symbols
+    public StockListAdapter(LinkedHashMap<String, ArrayList<Double>> data) {
         symbols = data;
     }
 
+    // Inflate the layout
     @NonNull
     @Override
     public StockViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Use the XML to inflate the layout, idk what the args do though
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.stock_item, parent, false);
 
@@ -35,25 +39,39 @@ public class StockListAdapter extends RecyclerView.Adapter<StockListAdapter.Stoc
         return stockViewHolder;
     }
 
+    // Binds the components in the view to actual data
     @Override
     public void onBindViewHolder(@NonNull StockViewHolder holder, int position) {
-        TextView symbol = holder.symbol;
+        // Bind each element in the holder to a member variable
+        TextView symbolText = holder.symbol;
+        TextView priceText = holder.price;
 
-        Log.v("Adapter", "Adding " + symbols.keySet().toArray()[position].toString());
-        symbol.setText(symbols.keySet().toArray()[position].toString());
+        // Translate the keys of the watchlist
+        String symbol = symbols.keySet().toArray()[position].toString();
+
+        // Set each text field to appropriate text, this method acts like a for loop
+        Log.v("Adapter", "Adding " + symbol);
+        symbolText.setText(symbol);
+        priceText.setText(symbols.get(symbol).get(0).toString());
     }
 
+    // Self-explanatory
     @Override
     public int getItemCount() {
         return symbols.size();
     }
 
+    // Holds the view for the recycler view and child components
     public class StockViewHolder extends RecyclerView.ViewHolder {
+        // Declare the two components of the recycler view
         TextView symbol;
+        TextView price;
 
+        // Constructor attaches the XML elements to these declared components
         public StockViewHolder(@NonNull View itemView) {
             super(itemView);
             this.symbol = (TextView) itemView.findViewById(R.id.stockText);
+            this.price = (TextView) itemView.findViewById(R.id.priceText);
         }
     }
 }
